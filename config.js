34e950a8116e0c22a31d7dbb85299f8cf57974c1
eashankumaaar00005-sudo <eashankumaaar00005@gmail.com>
2026-08-01 17:55:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,49 @@
+const CONFIG = {
 
+title: "A Secret Journey",
+
+receiver: "Someone Special ❤️",
+
+music: "assets/music/song.mp3",
+
+photos: [
+
+"assets/photos/1.jpg",
+
+"assets/photos/2.jpg",
+
+"assets/photos/3.jpg",
+
+"assets/photos/4.jpg",
+
+"assets/photos/5.jpg",
+
+"assets/photos/6.jpg"
+
+],
+
+letter: `
+
+Hi ❤️
+
+I made this little surprise
+just to make you smile.
+
+Keep scrolling...
+
+The best part
+is waiting for you.
+
+`,
+
+finalMessage: `
+
+Thank you
+for being part
+of this beautiful journey.
+
+❤️
+
+`
+
+};
